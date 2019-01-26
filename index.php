173ca24cf11575a2c9c4c99785ae5fe15fc4811f
JoseMaria <josemariagarciamoreno28@gmail.com>
2019-01-26 12:53:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php 
+	session_cache_limiter();
+	session_name("Usuario");
+	session_start();
 	include("funciones.php");
+
+	if (!isset($_SESSION["Usuario"])) {
+		$_SESSION["Usuario"] = "Invitado";
+	}
+
 	echo "<!DOCTYPE html>
 			<html>
 			<head>
@@ -9,6 +17,39 @@
 	echo "</head>
 			<body>";
 			menu();
+			sectionInicio();
+
+
+
+			if ($_SESSION["Usuario"] != "Invitado" && $_SESSION["Usuario"] != "admin") {
+						echo "<h1 class='display-1'>¡Bienvenido!</h1>";
+						echo "<br><br>
+						<form action='datosCliente.php' method='POST'>
+							 <input type='submit' class='btn btn-outline-dark btn-block' name='datos' value='Ver mis datos'>
+						</form>
+						<br>
+						<form action='comprasYpedidos.php' method='POST'>
+							 <input type='submit' class='btn btn-outline-dark btn-block' name='datos' value='Ver compras y pedidos'>
+						</form>";
+					}elseif($_SESSION["Usuario"] == "admin"){
+						echo "<h1 class='display-1'>¡Bienvenido! - ".$_SESSION["Usuario"]."</h1>";	
+						echo "<section class='container'>
+						<article class='row'>
+						<div class='col-md-12'>
+
+						<ul class='list-group'>
+						<li class='list-group-item menu'>Gestionar:</li>
+						<li class='list-group-item'><a href='usuarios.php'>Usuarios</a></li>
+						<li class='list-group-item'><a href='articulos.php'>Articulos</a></li>
+						</ul>
+
+						</div>
+						</article>";
+					}else{
+						echo "<h1 class='display-1'>¡Bienvenido!</h1>";
+					}
+
+			sectionFin();
 			
 	echo "</body>
 			</html>";
