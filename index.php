40711494dfab06ae8370fa8cf84d9161bb44ed6b
JoseMaria <josemariagarciamoreno28@gmail.com>
2019-01-26 12:35:42 +0100

    Update index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+	include("funciones.php");
+	echo "<!DOCTYPE html>
+			<html>
+			<head>
+				<meta charset='utf-8'>
+				<title>TiendaP</title>";
+				headi();
+	echo "</head>
+			<body>";
+			menu();
+			
+	echo "</body>
+			</html>";
+ ?>
