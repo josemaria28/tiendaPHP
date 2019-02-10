ad16182b2ff2a6a9143d8b8a4a708323a6deb997
JoseMaria <josemariagarciamoreno28@gmail.com>
2019-02-10 21:17:25 +0100

    Update confirmarPedido.php

--- confirmarPedido.php
+++ confirmarPedido.php
@@ -4,13 +4,23 @@
 	session_start();
 	include("funciones.php");
 
-	echo "<pre>";
+	/*echo "<pre>";
 	print_r($_SESSION["producto"]);
 	print_r($_SESSION["precio"]);
 	print_r($_SESSION["unidades"]);
-	echo "</pre>";
+	echo "</pre>";*/
+
+	// Insertar
+	$conexion = conectar("localhost","root","","bd_hardware");
+	
 
 	foreach ($_SESSION["producto"] as $indice=>$valor){
-
+		echo $_SESSION["producto"][$indice]."<br>";
+		echo $_SESSION["precio"][$indice]."<br>";
+		echo $_SESSION["unidades"][$indice]."<br>";
 	}
+
+
+
+	// header('Location: articulos.php');
  ?>
